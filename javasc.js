b42d35e9de899e75cadc2d5d42b6a782918d224a
nex-DoT <nex.dot2@gmail.com>
2023-02-07 09:33:32 +0330

    login page -v1.0

---
+++ javasc.js
@@ -0,0 +1,11 @@
+const btn =document.querySelector('.span');
+const form = document.querySelector('.inputs');
+
+function singup(){
+    btn.style.left="112px";
+    form.style.left="-262px"
+}
+function login(){
+    form.style.left="0px"
+    btn.style.left="0px"
+}
